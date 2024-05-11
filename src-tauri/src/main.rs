99e882c39e32ec23115478e45d1c1bea71b4a1e0
Himanshu Shekhar <hshekhar.dev@gmail.com>
2024-05-11 11:32:04 +0530

    implement layout and talk to tauri backend command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,34 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dotenv::dotenv;
+// use reqwest::Url;
+
+static WEATHER_API_URL: &str = "http://api.weatherapi.com/v1";
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn get_weather(location_name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", location_name)
+    dotenv().ok();
+
+    let weather_api_key: String =
+        std::env::var("WEATHER_API_KEY").expect("WEATHER_API_KEY must be set in the .env file!");
+
+    let mut current_weather_url = format!("{}", WEATHER_API_URL);
+
+    current_weather_url
+        .push_str(format!("/current.json?key={}&q={}", weather_api_key, location_name,).as_str());
+
+    println!("{}", current_weather_url);
+
+    // let response = reqwest::get(current_weather_url);
+
+    // println!(format!("{:?}", response));
+
+    format!(
+        "Getting weather data for {}\nRequestURL: {}",
+        location_name, current_weather_url
+    )
 }
 
 fn main() {
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
@@ -7,17 +6,69 @@ function App() {
   const [locationName, setLocationName] = useState("");
   const [weatherData, setWeatherData] = useState();
 
-  async function greet() {
+  async function getWeather(event: any) {
+    if (locationName.length < 1) return;
+    if (event.key && event.key !== "Enter") return;
     setWeatherData(await invoke("get_weather", { locationName }));
   }
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-9xl font-bold underline">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+    <div className="flex flex-col bg-gray-800 h-screen text-white rounded-xl">
+      <div
+        data-tauri-drag-region
+        className="titlebar flex justify-between pointer items-center p-2"
+      >
+        <div
+          className="text-xl font-bold pointer grow text-center"
+          data-tauri-drag-region
+        >
+          Bhayanak Weather
+        </div>
+        <div className="flex items-center fixed right-0 px-2">
+          <div className="titlebar-button p-2" id="titlebar-minimize">
+            <img
+              src="https://api.iconify.design/mdi:window-minimize.svg?color=white"
+              alt="minimize"
+              className="fill-white"
+            />
+          </div>
+          <div className="titlebar-button p-2" id="titlebar-maximize">
+            <img
+              src="https://api.iconify.design/mdi:window-maximize.svg?color=white"
+              alt="maximize"
+            />
+          </div>
+          <div className="titlebar-button p-2" id="titlebar-close">
+            <img
+              src="https://api.iconify.design/mdi:close.svg?color=white"
+              alt="close"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center mt-4 px-4 gap-2">
+        <SearchBar setLocationName={setLocationName} getWeather={getWeather} />
+        <WeatherDataComponent />
+        <div>{weatherData}</div>
+      </div>
     </div>
   );
 }
 
+function WeatherDataComponent() {
+  return <div className="border-2">WeatherData</div>;
+}
+function SearchBar(props: { setLocationName: any; getWeather: any }) {
+  return (
+    <input
+      type="text"
+      placeholder="Start entering city name, zipcode ip, metar, etc."
+      className="text-gray-800 w-full text-md p-2 rounded-full text-center border"
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+        props.setLocationName(event.target.value);
+      }}
+      onKeyDown={props.getWeather}
+    />
+  );
+}
 export default App;
